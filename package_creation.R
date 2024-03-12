f77c584fde1bde46909bd7af83720a767610c48b
Jonathan Rault <jonathan.rault@gmail.com>
2024-03-12 16:27:06 +0100

    remove useless instruction in package creation file

--- package_creation.R
+++ package_creation.R
@@ -12,11 +12,10 @@ usethis::use_mit_license(copyright_holder = NULL)
 usethis::use_r("dire_bonjour")
 usethis::use_package("glue")
 
-#usethis::use_data_raw("pays")
+usethis::use_data_raw("pays")
 usethis::use_r("data")
 
 dir.create("inst")
-usethis::add_logo("./data-raw/chaton.png")
 
 usethis::use_build_ignore("data-raw")
 usethis::use_build_ignore("package_creation.R")
